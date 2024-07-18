e5c477932f850faa0cd8365e9b51388ad5d3b0c3
Lemon <lemonconfidence101@gmail.com>
2024-07-18 17:20:37 +0100

    verification error

--- src/auth/Private.jsx
+++ src/auth/Private.jsx
@@ -1,17 +1,19 @@
 // src/components/PrivateRoute.js
-import { Navigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { Navigate, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function PrivateRoute({ element }) {
-    const isSignedIn = useSelector((state) => state.googleSignin);
-   
-   useEffect(() => {
-     return isSignedIn ? element : <Navigate to="/login" />;
-   }, [isSignedIn, element])
-   
-   
+  const isSignedIn = useSelector((state) => state.googleSignin);
+const navigate = useNavigate()
+  useEffect(() => {
+    if (isSignedIn == null) {
+      return element;
+    } else {
+      navigate ("/login");
+    }
+  }, [isSignedIn, element,navigate]);
 }
 
 PrivateRoute.propTypes = {
